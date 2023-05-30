658131d8c307860bf3cd5ac6756981f170a7cef8
Waihon Yew <yewwaihon@gmail.com>
2023-05-30 08:43:47 +0800

    Define GameTurn module

---
+++ game_turn.rb
@@ -0,0 +1,17 @@
+require_relative 'player'
+require_relative 'dice'
+
+module GameTun
+  def self.take_turn(player)
+    dice = Dice.new
+    case  dice.roll
+    when 1..2
+      player.blam
+    when 3..4
+      puts "#{player.name} was skipped."
+    when 5..6
+      player.w00t
+    end
+  end
+end
+
