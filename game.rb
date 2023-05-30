312e55d98c99390a393f65ecb0f17039410b809f
Waihon Yew <yewwaihon@gmail.com>
2023-05-31 00:15:05 +0800

    Save high scores to file

--- game.rb
+++ game.rb
@@ -41,13 +41,17 @@ class Game
     puts "#{player.name} (#{player.health})"
   end
 
-  def print_name_and_score(player)
+  def high_score_entry(player)
     formatted_name = player.name.ljust(20, '.')
-    puts "#{formatted_name} #{player.score}"
+    "#{formatted_name} #{player.score}"
+  end
+
+  def print_name_and_score(player)
+    puts high_score_entry(player)
   end
 
   def print_stats
-    puts "\n#{@title} Statustics:"
+    puts "\n#{@title} Statistics:"
 
     strong_players, wimpy_players = @players.partition { |player| player.strong? }
 
@@ -81,6 +85,15 @@ class Game
     puts "\nThere are #{treasures.size} treasures to be found:"
     treasures.each do |treasure|
       puts "A #{treasure.name} is worth #{treasure.points} points"
+    end
+  end
+
+  def save_high_scores(to_file="high_scores.txt")
+    File.open(to_file, "w") do |file|
+      file.puts "#{title} High Scores:"
+      @players.sort_by(&:score).each do |player|
+        file.puts high_score_entry(player)
+      end
     end
   end
 end
