fec5a5462d37302cb7d945fbcf3bd958ff43a637
Waihon Yew <yewwaihon@gmail.com>
2023-05-30 08:46:33 +0800

    Define play method for Game

--- game.rb
+++ game.rb
@@ -1,3 +1,6 @@
+require_relative 'player'
+require_relative 'game_turn'
+
 class Game
   attr_reader :title
 
@@ -8,5 +11,14 @@ class Game
 
   def  add_player(a_player)
     @players.push(a_player)
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{@title}:"
+
+    @players.each do |player|
+      GameTurn.take_turn(player)
+      puts player
+    end
   end
 end
